300cdc02219ebc9d5fb5cae64b497c13a4938921
FlorianMichael <florian.michael07@gmail.com>
2025-04-23 00:42:55 +0200

    Task 09

---
+++ src/de/javaeinstieg/A09.java
@@ -0,0 +1,37 @@
+package de.javaeinstieg;
+
+import java.util.Scanner;
+
+import static de.javaeinstieg.ScannerUtils.*;
+
+@SuppressWarnings("SpellCheckingInspection")
+public final class A09 {
+
+    private static final double MAX_LIMIT = -40.0;
+    private static double balance = 0.0;
+
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in); // Don't close it as it will run forever anyway:tm:
+        while (true) {
+            final int action = rangedInteger(scanner, 0, 2, "Möchten sie Geld einzahlen (0), auszahlen (1) oder einsehen (2)?");
+            if (action == 0) {                
+                final double add = rangedDecimal(scanner, 0, Double.MAX_VALUE, "Wie viel möchtest du einzahlen?");
+                balance += add;   
+                System.out.println("Dein Kontostand beträgt jetzt " + balance + " Euro."); 
+            } else if (action == 1) {
+                final double remove = getInput(
+                    scanner, 
+                    "Wie viel möchtest du abheben?",
+                    Double::parseDouble,
+                    value -> balance - value >= MAX_LIMIT,
+                    "Bitte gebe eine gültige Dezimalzahl ein, die das Kontolimit (" + MAX_LIMIT + ") nicht überschreitet."
+                );
+                balance -= remove;
+                System.out.println("Dein Kontostand beträgt jetzt " + balance + " Euro."); 
+            } else if (action == 2) {
+                System.out.println("Dein Kontostand beträgt " + balance + " Euro."); 
+            }
+        }
+    }
+
+}
--- src/de/javaeinstieg/ScannerUtils.java
+++ src/de/javaeinstieg/ScannerUtils.java
@@ -20,6 +20,13 @@ public final class ScannerUtils {
         return getInput(scanner, prompt, Integer::parseInt, value -> value > 0, "Bitte eine Zahl größer als null eingeben.");
     }
 
+    public static int rangedInteger(final Scanner scanner, final int min, final int max, final String prompt) {
+        if (max <= min) {
+            throw new IllegalStateException("Invalid range parameters. Expecting max > min");
+        }
+        return getInput(scanner, prompt, Integer::parseInt, value -> value >= min && value <= max, "Bitte eine Zahl zwischen " + min + " und " + max + " eingeben.");
+    }
+
     public static double decimal(final Scanner scanner, final String prompt) {
         return getValidatedInput(scanner, prompt, Double::parseDouble, "Ungültige Dezimalzahl. Bitte erneut eingeben.");
     }
@@ -28,10 +35,17 @@ public final class ScannerUtils {
         return getInput(scanner, prompt, Double::parseDouble, value -> value > 0, "Bitte eine Dezimalzahl größer als null eingeben.");
     }
 
+    public static double rangedDecimal(final Scanner scanner, final double min, final double max, final String prompt) {
+        if (max <= min) {
+            throw new IllegalStateException("Invalid range parameters. Expecting max > min");
+        }
+        return getInput(scanner, prompt, Double::parseDouble, value -> value >= min && value <= max, "Bitte eine Dezimalzahl zwischen " + min + " und " + max + " eingeben.");
+    }
+
     private static <T> T getValidatedInput(final Scanner scanner, final String prompt, final Parser<T> parser, final String errorMessage) {
         while (true) {
             System.out.print(prompt + " ");
-            final String input = scanner.nextLine();
+            String input = scanner.nextLine();
             try {
                 return parser.parse(input);
             } catch (NumberFormatException e) {
@@ -40,7 +54,7 @@ public final class ScannerUtils {
         }
     }
 
-    private static <T> T getInput(final Scanner scanner, final String prompt, final Parser<T> parser, final Validator<T> validator, final String errorMessage) {
+    public static <T> T getInput(final Scanner scanner, final String prompt, final Parser<T> parser, final Validator<T> validator, final String errorMessage) {
         while (true) {
             final T value = getValidatedInput(scanner, prompt, parser, errorMessage);
             if (validator.isValid(value)) {
@@ -52,14 +66,14 @@ public final class ScannerUtils {
     }
 
     @FunctionalInterface
-    private interface Parser<T> {
+    public interface Parser<T> {
 
         T parse(final String input) throws NumberFormatException;
 
     }
 
     @FunctionalInterface
-    private interface Validator<T> {
+    public interface Validator<T> {
 
         boolean isValid(final T value);
 
